99ac110d76e15662d89080f43411abac26fed207
Davide Domenghini <davidedomenghini@Davides-MacBook-Air.local>
2020-10-02 12:47:02 +0100

    changes on home and inputs fields

--- src/pages/CheckSymptoms.tsx
+++ src/pages/CheckSymptoms.tsx
@@ -16,12 +16,12 @@ import React from 'react';
 import Continue from '../components/Continue';
 
 const checkboxList = [
-  { val: 'a high temperature', isChecked: false },
-  { val: 'a new cough', isChecked: false },
-  { val: 'a loss or change to your sense of smell or taste', isChecked: false },
-  { val: 'shortness of breath', isChecked: false }
+  { val: 'a high temperature', desc:'a high temperature - this means you feel hot to touch on your chest or back (you do not need to measure your temperature)' ,  isChecked: false },
+  { val: 'a new cough', desc: 'this means coughing a lot for more than an hour, or 3 or more coughing episodes in 24 hours (if you usually have a cough, it may be worse than usual)', isChecked: false },
+  { val: 'a loss or change to your sense of smell or taste', desc: 'this means you have noticed you cannot smell or taste anything, or things smell or taste different to normal', isChecked: false },
+  { val: 'shortness of breath', desc: 'uncomfortable feeling of being out of breath', isChecked: false }
 ];
-
+ 
 const CheckSymptoms: React.FC = () => {
   return (
     <IonPage>
@@ -33,10 +33,13 @@ const CheckSymptoms: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding" fullscreen>
         <IonList>
+        <IonItemDivider></IonItemDivider>
           <IonItemDivider>Please make sure you are resting for at least 5 minutes before taking your readings.</IonItemDivider>
-          {checkboxList.map(({ val, isChecked }, i) => (
-            <IonItem key={i}>
-              <IonLabel>{val}</IonLabel>
+          <IonItemDivider></IonItemDivider>
+          {checkboxList.map(({ val, desc, isChecked }, i) => (
+            
+              <IonItem key={i}>
+              <IonLabel>{val} <h3>{desc}</h3></IonLabel> 
               <IonCheckbox slot="end" color="danger" value={val} checked={isChecked} />
             </IonItem>
           ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,13 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
+      <h3>Ring your GP/NHS 111 as soon as possible if:</h3>
+
+      <ul className="warning-list">
+          <li>You slowly start feeling more unwell or more breathless for two or more hours. </li>
+          <li>You are having difficulty breathing when getting up to go to the toilet or similar.</li>
+          <li>You sense that something is wrong (general weakness, extreme tiredness, loss of appetite, reduced urine output, unable to care for yourself – simple tasks like washing and dressing or making food).</li>
+      </ul>
         <ExploreContainer />
       </IonContent>
     </IonPage>
